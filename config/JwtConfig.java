ec9dc62b608068e9f758c2dd3d1d1d23efec7549
ArSingla <147928296+ArSingla@users.noreply.github.com>
2025-09-14 19:28:42 +0530

    Add files via upload

---
+++ config/JwtConfig.java
@@ -0,0 +1,8 @@
+package com.petcare.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JwtConfig {
+    // JWT configuration is handled in SecurityConfig and JwtTokenProvider
+}
---
+++ config/MongoConfig.java
@@ -0,0 +1,5 @@
+package main.java.com.petcare.config;
+
+public class MongoConfig {
+    
+}
